1d41e23a8e3ac2e7842b3e2408c79c089b3500c4
magmacrunchmedia <magmacrunchmedia@gmail.com>
2026-08-23 22:08:37 -0400

    Update texastoast to 0.1.2

--- Formula/texastoast.rb
+++ Formula/texastoast.rb
@@ -3,8 +3,8 @@ class Texastoast < Formula
 
   desc "Python RPG engine and Magma Hub I2C layer for magmacrunch game systems"
   homepage "https://github.com/magmacrunchmedia/texastoast"
-  url "https://files.pythonhosted.org/packages/5c/14/8f1d1697b83cc424de3303b8a1bb4bc1ece6d207d7464daec71734867943/texastoast-0.1.1.tar.gz"
-  sha256 "1002250e50cd6882d4b51d466b971ade9a00a8cd71d03254a7e51fb6e6139f82"
+  url "https://files.pythonhosted.org/packages/4d/ff/4d6a891defd52a5aa3c1607da3937a5f775bb48d8138a01281801b46a99b/texastoast-0.1.2.tar.gz"
+  sha256 "117363fc4b10cb8a60bbdf2b0c9fe30368bf383f3fd4ef51cddf878861107734"
   license "Apache-2.0"
 
   depends_on "python@3.12"
