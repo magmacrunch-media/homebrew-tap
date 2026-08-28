eee5a895739864867ec5e1812e703a96ac7fa376
magmacrunchmedia <magmacrunchmedia@gmail.com>
2026-08-28 16:03:20 -0400

    texastoast 0.11.0

--- Formula/texastoast.rb
+++ Formula/texastoast.rb
@@ -3,8 +3,8 @@ class Texastoast < Formula
 
   desc "Python RPG engine with I2C hardware abstraction for magmacrunch game systems"
   homepage "https://github.com/magmacrunchmedia/texastoast"
-  url "https://files.pythonhosted.org/packages/85/43/5ef7e93ab84a244e99d01cd60c0b085ac8f313e40764291f339211ad37a4/texastoast-0.1.3.tar.gz"
-  sha256 "0ddb5be097eaf36adc0ecf5aef37f979c165bd462c8ff77107592d21d141f990"
+  url "https://files.pythonhosted.org/packages/32/c3/a1cd1cb1f0a3c95f3f20cf8a7ae661107b605b3bbdfeaad39558d3448662/texastoast-0.11.0.tar.gz"
+  sha256 "0af1ce24da3092c5c8ffa402bd110450fb0cc0f977a5c3f892745e2fd37258ef"
   license "Apache-2.0"
 
   depends_on "python@3.12"
