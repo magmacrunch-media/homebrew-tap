93d8923c19d3689c8d49b2c9a5aa27bb82b8dd82
magmacrunch media <magmacrunchmedia@gmail.com>
2026-08-29 01:08:11 +0000

    texastoast: add 0.11.0 bottle.

--- Formula/texastoast.rb
+++ Formula/texastoast.rb
@@ -7,6 +7,14 @@ class Texastoast < Formula
   sha256 "0af1ce24da3092c5c8ffa402bd110450fb0cc0f977a5c3f892745e2fd37258ef"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/magmacrunchmedia/homebrew-tap/releases/download/texastoast-0.11.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cabbb1b2f24dc87585fea3d39aa8ca0d914e57af937abcf901139f6786acdf66"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e68dbf475a566c0d2b5467acfc4cdc25bdb851ecae9ee7c4e073ffb1dca7da9e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "81bbdc0a8c6661ae3e28a26008ce643a86ed7e56ec756c68b2d7a4f87a5c6a20"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "018ed014f4205124eb14c6fcce4c9006990409d416c9b6c5fe6ef364ded9aac4"
+  end
+
   depends_on "python@3.12"
 
   def install
